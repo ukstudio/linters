d8fc8e699ebfc2e3bb849d9c77176cc7d0d03c38
Juan Ignacio Donoso <jidonoso@gmail.com>
2017-01-26 10:01:33 -0500

    Adds tslint tokenizer spec

---
+++ spec/lib/linters/tslint/tokenizer_spec.rb
@@ -0,0 +1,16 @@
+require "linters/tokenizer"
+require "linters/tslint/tokenizer"
+
+describe Linters::Tslint::Tokenizer do
+  describe "#parse" do
+    it "parses line numbers when columns provided" do
+      input = "test.ts[1, 1]: require statement not part of an import statement"
+      expected_message = "require statement not part of an import statement"
+      tokenizer = Linters::Tslint::Tokenizer.new
+
+      parsed = tokenizer.parse(input)
+
+      expect(parsed).to eq([{ line: 1, message: expected_message }])
+    end
+  end
+end
